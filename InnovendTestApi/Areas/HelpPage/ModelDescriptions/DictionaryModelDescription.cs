a3866744be2adebf56b2f08f07bf7f0a57b5b750
MartinInnovend <60383695+MartinInnovend@users.noreply.github.com>
2020-08-12 14:24:18 +0200

    First Commit All

---
+++ InnovendTestApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InnovendTestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ InnovendTestWebApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InnovendTestWebApp1.MvcApplication" Language="C#" %>
